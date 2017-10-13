e6dcdfc29a62ab6e6b5699cba6ba16bed1966c1f
Thomas Christerus <tchristerus@outlook.com>
2017-10-13 13:36:22 +0200

    added bluetooth test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,55 @@
+import javax.bluetooth.*;
+import java.io.IOException;
+import java.util.Vector;
+
 /**
  * Created by on 12/10/2017.
  */
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Works :)");
+    public static final Vector/*<RemoteDevice>*/ devicesDiscovered = new Vector();
+
+    public static void main(String[] args) throws InterruptedException {
+        final Object inquiryCompletedEvent = new Object();
+
+        devicesDiscovered.clear();
+
+        DiscoveryListener listener = new DiscoveryListener() {
+
+            public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
+                System.out.println("Device " + btDevice.getBluetoothAddress() + " found");
+                devicesDiscovered.addElement(btDevice);
+                try {
+                    System.out.println("     name " + btDevice.getFriendlyName(false));
+                } catch (IOException cantGetDeviceName) {
+                }
+            }
+
+            public void inquiryCompleted(int discType) {
+                System.out.println("Device Inquiry completed!");
+                synchronized(inquiryCompletedEvent){
+                    inquiryCompletedEvent.notifyAll();
+                }
+            }
+
+            public void serviceSearchCompleted(int transID, int respCode) {
+            }
+
+            public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
+            }
+        };
+
+        synchronized(inquiryCompletedEvent) {
+            boolean started = false;
+            try {
+                started = LocalDevice.getLocalDevice().getDiscoveryAgent().startInquiry(DiscoveryAgent.GIAC, listener);
+            } catch (BluetoothStateException e) {
+                e.printStackTrace();
+            }
+            if (started) {
+                System.out.println("wait for device inquiry to complete...");
+                inquiryCompletedEvent.wait();
+                System.out.println(devicesDiscovered.size() +  " device(s) found");
+            }
+        }
     }
 }
