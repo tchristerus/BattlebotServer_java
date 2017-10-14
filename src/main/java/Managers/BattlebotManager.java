b37d5ebf48e0d6bf221b868d23298de4be098e90
tchristerus <tchristerus@outlook.com>
2017-10-14 17:58:26 +0200

    Changed the ArrayList to ObservableList which has listeners build in

--- src/main/java/Managers/BattlebotManager.java
+++ src/main/java/Managers/BattlebotManager.java
@@ -1,6 +1,10 @@
 package Managers;
 
 import Models.Battlebot;
+import com.intel.bluetooth.btspp.Connection;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 
 import javax.bluetooth.RemoteDevice;
 import java.io.IOException;
@@ -8,10 +12,20 @@ import java.util.ArrayList;
 
 public class BattlebotManager {
     private BluetoothManager bluetoothManager;
-    private ArrayList<Battlebot> battlebots = new ArrayList<>();
+    private ObservableList<Battlebot> battlebots = FXCollections.observableList(new ArrayList<>());
 
     public BattlebotManager(){
         bluetoothManager = new BluetoothManager(this);
+
+        battlebots.addListener(new ListChangeListener<Battlebot>() {
+            @Override
+            public void onChanged(Change<? extends Battlebot> c) {
+                // fires when new bots are added
+                battlebots.forEach(battlebot -> {
+                    // TODO THE DEVICE IS FOUND BUT IT SEEMS LIKE IT DOES NOT HAVE ANY CONNECITON YET
+                    System.out.println("Authenticated: " + battlebot.getRemoteDevice().isAuthenticated());});
+            }
+        });
     }
 
     public void createBattlebot(RemoteDevice remoteDevice){
