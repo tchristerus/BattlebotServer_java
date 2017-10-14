b9302c53fdf2b0908986ec0e91c098122f54ff4a
tchristerus <tchristerus@outlook.com>
2017-10-14 14:37:30 +0200

    Created SocketIO server
    changed language level to 8.0 (lambda expressions)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
+import Managers.SocketManager;
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketConfig;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
+
 import javax.bluetooth.*;
 import java.io.IOException;
 import java.util.Vector;
@@ -9,50 +16,12 @@ import java.util.Vector;
 
 
 public class Main {
-    public static final Vector/*<RemoteDevice>*/ devicesDiscovered = new Vector();
 
-    public static void main(String[] args) throws InterruptedException {
-        final Object inquiryCompletedEvent = new Object();
+    public static SocketManager socketManager;
 
-        devicesDiscovered.clear();
+    public static void main(String[] args) {
+        socketManager = new SocketManager("localhost", 1337);
 
-        DiscoveryListener listener = new DiscoveryListener() {
-
-            public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
-                System.out.println("Device " + btDevice.getBluetoothAddress() + " found");
-                devicesDiscovered.addElement(btDevice);
-                try {
-                    System.out.println("     name " + btDevice.getFriendlyName(false));
-                } catch (IOException cantGetDeviceName) {
-                }
-            }
-
-            public void inquiryCompleted(int discType) {
-                System.out.println("Device Inquiry completed!");
-                synchronized(inquiryCompletedEvent){
-                    inquiryCompletedEvent.notifyAll();
-                }
-            }
-
-            public void serviceSearchCompleted(int transID, int respCode) {
-            }
-
-            public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
-            }
-        };
-
-        synchronized(inquiryCompletedEvent) {
-            boolean started = false;
-            try {
-                started = LocalDevice.getLocalDevice().getDiscoveryAgent().startInquiry(DiscoveryAgent.GIAC, listener);
-            } catch (BluetoothStateException e) {
-                e.printStackTrace();
-            }
-            if (started) {
-                System.out.println("wait for device inquiry to complete...");
-                inquiryCompletedEvent.wait();
-                System.out.println(devicesDiscovered.size() +  " device(s) found");
-            }
-        }
+        socketManager.startServer();
     }
 }
---
+++ src/main/java/Managers/SocketManager.java
@@ -0,0 +1,46 @@
+package Managers;
+
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketConfig;
+import com.corundumstudio.socketio.SocketIOServer;
+
+public class SocketManager {
+    private Configuration config;
+    private SocketConfig socketConfig;
+    private SocketIOServer socketServer;
+
+    public SocketManager(String hostname, int port){
+        config = new Configuration();
+        config.setHostname(hostname);
+        config.setPort(port);
+
+        socketConfig = new SocketConfig();
+        socketConfig.setReuseAddress(true);
+
+        config.setSocketConfig(socketConfig);
+
+        socketServer = new SocketIOServer(config);
+        registerEvents();
+
+    }
+
+    public void startServer(){
+        socketServer.start();
+    }
+
+    public void stopServer(){
+        socketServer.stop();
+    }
+
+    public void registerEvents(){
+        socketServer.addConnectListener(socketIOClient -> sendToAllClients("hi", "Someone connected: " + socketIOClient.getSessionId()));
+    }
+
+    public void sendToAllClients(String event, Object data){
+        socketServer.getAllClients().forEach(client -> client.sendEvent(event, data));
+    }
+
+    public SocketIOServer getSocketServer() {
+        return socketServer;
+    }
+}
