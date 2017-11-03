7b598aea8c4d25138b1f57c4b7a9bd47f09ab8e4
Thomas Christerus <tchristerus@outlook.com>
2017-11-03 17:41:38 +0100

    -untested- Wrapped all the code with an anonymous function
    Added this to prevent console injections.

--- web/index.js
+++ web/index.js
@@ -1,135 +1,136 @@
-var socket = io.connect('http://battlebot.serverict.nl:6969');
-var latestUpdate = new Array();
-//  addOrUpdateBot(testBot);
+(function(){
+  var socket = io.connect('http://battlebot.serverict.nl:6969');
+  var latestUpdate = new Array();
+  //  addOrUpdateBot(testBot);
 
-function timeoutTimer(){
+  function timeoutTimer(){
 
-    socket.emit("uptime", "");
+      socket.emit("uptime", "");
 
-    for (var i in latestUpdate){
-        if (typeof latestUpdate[i] !== 'function') {
-            if(Date.now() - latestUpdate[i] > 5000){
-                $('#table_bots > tbody > tr[mac="'+i+'"]').addClass('danger');
-                $('#table_bots > tbody > tr[mac="'+i+'"]').attr('disabled',true);
-                $('#table_bots > tbody > tr[mac="'+i+'"]').find("td").eq(4).find('button').fadeIn();
-            }else{
-                $('#table_bots > tbody > tr[mac="'+i+'"]').removeClass('danger');
-                $('#table_bots > tbody > tr[mac="'+i+'"]').attr('disabled',false);
-                $('#table_bots > tbody > tr[mac="'+i+'"]').find("td").eq(4).find('button').fadeOut();
+      for (var i in latestUpdate){
+          if (typeof latestUpdate[i] !== 'function') {
+              if(Date.now() - latestUpdate[i] > 5000){
+                  $('#table_bots > tbody > tr[mac="'+i+'"]').addClass('danger');
+                  $('#table_bots > tbody > tr[mac="'+i+'"]').attr('disabled',true);
+                  $('#table_bots > tbody > tr[mac="'+i+'"]').find("td").eq(4).find('button').fadeIn();
+              }else{
+                  $('#table_bots > tbody > tr[mac="'+i+'"]').removeClass('danger');
+                  $('#table_bots > tbody > tr[mac="'+i+'"]').attr('disabled',false);
+                  $('#table_bots > tbody > tr[mac="'+i+'"]').find("td").eq(4).find('button').fadeOut();
 
-            }
-        }
-    }
-};
+              }
+          }
+      }
+  };
 
+  setInterval(timeoutTimer, 1000);
+  socket.on('connected', function (data) {
+      $("h1").html('Connection established')
+  });
 
-setInterval(timeoutTimer, 1000);
-socket.on('connected', function (data) {
-    $("h1").html('Connection established')
-});
+  socket.on('update_bot', function(data){
+      addOrUpdateBot(data);
+  });
 
-socket.on('update_bot', function(data){
-    addOrUpdateBot(data);
-});
+  socket.on('uptime', function(data){
+      $("#time").html("Server uptime: " + data)
+  });
 
-socket.on('uptime', function(data){
-    $("#time").html("Server uptime: " + data)
-});
+  socket.on('disconnect', function(data){
+      $("h1").html(data)
+  });
 
-socket.on('disconnect', function(data){
-    $("h1").html(data)
-});
-
-socket.on('console_message', function(data){
-    $('#console').append("<tr><td>> " + data + "</td></tr>")
-    $(".console").stop().animate({ scrollTop: $(".console")[0].scrollHeight}, 1000);
-});
-
-
-
-function sendCharToBot(botname, char){
-    socket.emit("send", "{bot: " + botname + ", message: " + char + "}")
-}
-// als verbinden failed (server kant) dan moet er een event gestuurd worden naar de client dat het niet is gelukt en de knop weer enabled moet / text updaten van de knop
-
-socket.on('connection_failed', function(data){
-    var jsonobj = $.parseJSON(data);
-    $("button#" + jsonobj.mac).html("Opniew verbinden");
-    $("button#" + jsonobj.mac).attr("disabled", false);
-});
-
-$(document).ready(function(){
-    $("#btnConnect").click(function(){
-        reconnect($("#connectBot").val(), null);
-    });
-    addControlerButton("up", 1, 38);
-    addControlerButton("right", 2, 39);
-    addControlerButton("down", 3, 40);
-    addControlerButton("left", 4, 37);
-
-    function addControlerButton(id, action, keyboardKey){
-        $("#" + id).mousedown(function () {
-            sendCharToBot($("#botNameField").val(),action);
-        });
-
-        $("#" + id).mouseup(function () {
-            sendCharToBot($("#botNameField").val(),5);
-        });
-    }
-
-});
-
-$("body").keydown(function (data) {
-    switch(data.which){
-        case 38:
-            sendCharToBot($("#botNameField").val(),action);
-            break;
-    }
-});
-$("body").keyup(function (data) {
-    sendCharToBot($("#botNameField").val(),5);
-});
+  socket.on('console_message', function(data){
+      $('#console').append("<tr><td>> " + data + "</td></tr>")
+      $(".console").stop().animate({ scrollTop: $(".console")[0].scrollHeight}, 1000);
+  });
 
 
-function close(){
-    socket.emit("close", "");
-    console.info("Close command send...")
-}
 
-function search(){
-    socket.emit("search", "");
-    console.info("Search command send...")
-}
-function reconnect(name, mac){
-    socket.emit("reconnectEvent", name);
-    if(mac){
-        $("button#" +mac).html("Verbinden...");
-        $("button#" +mac).attr("disabled", true);
-    }
-}
+  function sendCharToBot(botname, char){
+      socket.emit("send", "{bot: " + botname + ", message: " + char + "}")
+  }
+  // als verbinden failed (server kant) dan moet er een event gestuurd worden naar de client dat het niet is gelukt en de knop weer enabled moet / text updaten van de knop
 
-function addOrUpdateBot(jsonString){
-    var jsonobj = $.parseJSON(jsonString)
-    var alreadyExist = false;
+  socket.on('connection_failed', function(data){
+      var jsonobj = $.parseJSON(data);
+      $("button#" + jsonobj.mac).html("Opniew verbinden");
+      $("button#" + jsonobj.mac).attr("disabled", false);
+  });
 
-    // Searching for existing row with this mac
-    $('#table_bots > tbody  > tr').each(function() {
-        if($(this).attr("mac") == jsonobj.mac){
-            $(this).find("td").eq(1).html(jsonobj.speed);
-            $(this).find("td").eq(2).html(jsonobj.distance);
-            $(this).find("td").eq(3).html(jsonobj.time);
-            $("button#" +jsonobj.mac).html("Opnieuw verbinden");
-            $("button#" +jsonobj.mac).attr("disabled", false);
-            alreadyExist = true;
-            latestUpdate[jsonobj.mac] = Date.now();
-        }
-    });
+  $(document).ready(function(){
+      $("#btnConnect").click(function(){
+          reconnect($("#connectBot").val(), null);
+      });
+      addControlerButton("up", 1, 38);
+      addControlerButton("right", 2, 39);
+      addControlerButton("down", 3, 40);
+      addControlerButton("left", 4, 37);
 
-    //console.info(alreadyExist);
-    // Bot did not yet exist... creating new row
-    if(!alreadyExist){
-        //console.info('<tr mac="' + jsonobj.mac + '"><td>' + jsonobj.name + '</td><td>' + jsonobj.speed + '</td><td>' + jsonobj.distance + '</td><td>' + jsonobj.time + '</td></tr>');
-        $('#table_bots > tbody').append('<tr mac="' + jsonobj.mac + '"><td>' + jsonobj.name + '</td><td>' + jsonobj.speed + '</td><td>' + jsonobj.distance + '</td><td>' + jsonobj.time + '</td><td> <button class="btn btn-info" id=\''+ jsonobj.mac + '\' onclick="reconnect(\''+ jsonobj.name +'\', this.id)">Opnieuw verbinden</button></td></tr>');
-        latestUpdate[jsonobj.mac] = Date.now();
-    }
-}
+      function addControlerButton(id, action, keyboardKey){
+          $("#" + id).mousedown(function () {
+              sendCharToBot($("#botNameField").val(),action);
+          });
+
+          $("#" + id).mouseup(function () {
+              sendCharToBot($("#botNameField").val(),5);
+          });
+      }
+
+  });
+
+  $("body").keydown(function (data) {
+      switch(data.which){
+          case 38:
+              sendCharToBot($("#botNameField").val(),action);
+              break;
+      }
+  });
+  $("body").keyup(function (data) {
+      sendCharToBot($("#botNameField").val(),5);
+  });
+
+
+  function close(){
+      socket.emit("close", "");
+      console.info("Close command send...")
+  }
+
+  function search(){
+      socket.emit("search", "");
+      console.info("Search command send...")
+  }
+  function reconnect(name, mac){
+      socket.emit("reconnectEvent", name);
+      if(mac){
+          $("button#" +mac).html("Verbinden...");
+          $("button#" +mac).attr("disabled", true);
+      }
+  }
+
+  function addOrUpdateBot(jsonString){
+      var jsonobj = $.parseJSON(jsonString)
+      var alreadyExist = false;
+
+      // Searching for existing row with this mac
+      $('#table_bots > tbody  > tr').each(function() {
+          if($(this).attr("mac") == jsonobj.mac){
+              $(this).find("td").eq(1).html(jsonobj.speed);
+              $(this).find("td").eq(2).html(jsonobj.distance);
+              $(this).find("td").eq(3).html(jsonobj.time);
+              $("button#" +jsonobj.mac).html("Opnieuw verbinden");
+              $("button#" +jsonobj.mac).attr("disabled", false);
+              alreadyExist = true;
+              latestUpdate[jsonobj.mac] = Date.now();
+          }
+      });
+
+      //console.info(alreadyExist);
+      // Bot did not yet exist... creating new row
+      if(!alreadyExist){
+          //console.info('<tr mac="' + jsonobj.mac + '"><td>' + jsonobj.name + '</td><td>' + jsonobj.speed + '</td><td>' + jsonobj.distance + '</td><td>' + jsonobj.time + '</td></tr>');
+          $('#table_bots > tbody').append('<tr mac="' + jsonobj.mac + '"><td>' + jsonobj.name + '</td><td>' + jsonobj.speed + '</td><td>' + jsonobj.distance + '</td><td>' + jsonobj.time + '</td><td> <button class="btn btn-info" id=\''+ jsonobj.mac + '\' onclick="reconnect(\''+ jsonobj.name +'\', this.id)">Opnieuw verbinden</button></td></tr>');
+          latestUpdate[jsonobj.mac] = Date.now();
+      }
+  }
+})();
